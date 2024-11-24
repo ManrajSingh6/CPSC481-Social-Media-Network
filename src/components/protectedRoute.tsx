2edc641beb87083ef5929bee20f4d53b2cf2e970
Manraj Singh <manrajs2025@gmail.com>
2024-11-24 00:24:21 -0700

    Minor update to user context and protected route

--- src/components/protectedRoute.tsx
+++ src/components/protectedRoute.tsx
@@ -3,7 +3,11 @@ import { useUser } from '../context/userContext'
 import { LOGIN_ROUTE } from '../utils/routes'
 
 export function ProtectedRoute(): JSX.Element {
-  const { user } = useUser()
+  const { user, isUserLoading } = useUser()
+
+  if (isUserLoading) {
+    return <p>Loading...</p>
+  }
 
   return user ? <Outlet /> : <Navigate to={LOGIN_ROUTE} />
 }
--- src/context/userContext.tsx
+++ src/context/userContext.tsx
@@ -1,32 +1,50 @@
-import { createContext, useContext, useState } from 'react'
+import { createContext, useContext, useEffect, useState } from 'react'
 import { User } from '../utils/types'
 import { MOCK_USER } from '../utils/mockData'
+
+const LOCAL_STORAGE_USER_KEY = 'logged_in_user'
 
 interface UserContextType {
   readonly user: User | null
   readonly login: (username: string, password: string) => boolean
+  readonly isUserLoading: boolean
 }
 
 interface UserProviderProps {
   children: React.ReactNode
 }
 
-export const UserContext = createContext<UserContextType | null>(null)
+const UserContext = createContext<UserContextType | null>(null)
 
 export function UserProvider({ children }: UserProviderProps): JSX.Element {
   const [user, setUser] = useState<User | null>(null)
+  const [isUserLoading, setIsUserLoading] = useState(true)
 
   function login(username: string, password: string): boolean {
     if (username === MOCK_USER.username && password === MOCK_USER.password) {
       setUser(MOCK_USER)
+      localStorage.setItem(LOCAL_STORAGE_USER_KEY, JSON.stringify(MOCK_USER))
       return true
     }
 
     return false
   }
 
+  useEffect(() => {
+    setIsUserLoading(true)
+    const existingLoggedInUser = localStorage.getItem(LOCAL_STORAGE_USER_KEY)
+
+    console.log('existingLoggedInUser', existingLoggedInUser)
+
+    if (existingLoggedInUser) {
+      setUser(JSON.parse(existingLoggedInUser))
+    }
+
+    setIsUserLoading(false)
+  }, [])
+
   return (
-    <UserContext.Provider value={{ user, login }}>
+    <UserContext.Provider value={{ user, login, isUserLoading }}>
       {children}
     </UserContext.Provider>
   )
