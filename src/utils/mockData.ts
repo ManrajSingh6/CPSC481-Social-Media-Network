7b5b3cb8056468caba6143ab63da84c138003abc
Manraj Singh <manrajs2025@gmail.com>
2024-11-24 00:07:00 -0700

    Add mock user and type

---
+++ src/utils/mockData.ts
@@ -0,0 +1,9 @@
+import { User } from './types'
+
+export const MOCK_USER: User = {
+  username: 'jdoe',
+  email: 'jdoe@mail.com',
+  password: 'jdoe',
+  fullName: 'John Doe',
+  profilePicUrl: 'https://avatar.iran.liara.run/public/5'
+}
---
+++ src/utils/types.ts
@@ -0,0 +1,7 @@
+export interface User {
+  readonly username: string
+  readonly email: string
+  readonly password: string
+  readonly fullName: string
+  readonly profilePicUrl: string
+}
