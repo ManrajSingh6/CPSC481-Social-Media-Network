ab53553a60b09d5cce959a79d4da777d9dde1c26
Manraj Singh <manrajs2025@gmail.com>
2024-11-24 00:29:11 -0700

    Add WIP mock login functionality (hardcoded)

--- src/pages/LoginPage.tsx
+++ src/pages/LoginPage.tsx
@@ -1,13 +1,41 @@
 import '../assets/styles.css'
 import { useNavigate } from 'react-router-dom'
 import { Link } from 'react-router-dom'
-import { REGISTER_ROUTE } from '../utils/routes'
+import { GROUPS_ROUTE, REGISTER_ROUTE } from '../utils/routes'
+import { useState } from 'react'
+import { MOCK_USER } from '../utils/mockData'
+import { useUser } from '../context/userContext'
+
+interface LoginData {
+  readonly username: string
+  readonly password: string
+}
 
 function LoginPage() {
   const navigate = useNavigate()
+
+  // TODO: Implement login functionality with setLoginData
+  const [loginData, setLoginData] = useState<LoginData>({
+    username: MOCK_USER.username,
+    password: MOCK_USER.password
+  })
+
+  const { login: loginUser } = useUser()
+
   const handleRegister = () => {
     navigate(REGISTER_ROUTE)
   }
+
+  function handleLogin() {
+    const success = loginUser(loginData.username, loginData.password)
+
+    if (success) {
+      navigate(GROUPS_ROUTE)
+    } else {
+      alert('Invalid login') // TODO: use nicer alerting mechanism
+    }
+  }
+
   return (
     <div className='login-container'>
       <h1 className='header'>Welcome Back :{')'}</h1>
@@ -15,16 +43,28 @@ function LoginPage() {
 
       <div className='form-container'>
         <p className='label'>Username</p>
-        <input type='text' className='input' placeholder='Enter username' />
+        <input
+          type='text'
+          className='input'
+          placeholder='Enter username'
+          value={loginData.username}
+        />
         <p className='label'>Password</p>
-        <input type='password' className='input' placeholder='Password' />
+        <input
+          type='password'
+          className='input'
+          placeholder='Password'
+          value={loginData.password}
+        />
         <Link
           to='/forgot'
           className='flex-start text-blue-500 underline hover:text-blue-700'
         >
           Forgot Password?
         </Link>
-        <button className='register-button'>Login</button>
+        <button className='register-button' onClick={handleLogin}>
+          Login
+        </button>
         <p className='label'>
           <input style={{ margin: '5px' }} type='checkbox' />
           Remember me
@@ -44,7 +84,6 @@ function LoginPage() {
       >
         Register
       </button>
-
       <div className='footer'>
         <span>Terms & Conditions</span>
         <span>About Us</span>
