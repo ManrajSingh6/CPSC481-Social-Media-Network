c58a8f2aa5d1d2886690acf1da0e1ea6952d4a1f
Manraj Singh <manrajs2025@gmail.com>
2024-11-24 00:28:46 -0700

    Build page header

---
+++ src/components/pageHeader.tsx
@@ -0,0 +1,20 @@
+import { Bell } from 'lucide-react'
+import { User } from '../utils/types'
+import { CustomImage } from './customImage'
+
+interface PageHeaderProps {
+  readonly user: User
+}
+
+export function PageHeader({ user }: PageHeaderProps): JSX.Element {
+  return (
+    <div className='flex items-center justify-between p-4'>
+      <CustomImage
+        src={user.profilePicUrl}
+        alt='user-profile-pic'
+        className='h-14 w-14 rounded-full shadow-md'
+      />
+      <Bell className='h-6 w-6' />
+    </div>
+  )
+}
